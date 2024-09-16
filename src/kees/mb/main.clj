f93a7ec42e856d111b898dba0f34c4b5a858486c
kees- <6820950+kees-@users.noreply.github.com>
2024-09-15 17:21:50 -0700

    Begin templating

--- src/kees/mb/main.clj
+++ src/kees/mb/main.clj
@@ -1,6 +1,23 @@
 (ns kees.mb.main
-  (:require [kees.mb.data :as data]
-            [kees.mb.transform :as transform]))
+  (:require [clojure.pprint]
+            [kees.mb.data :as data]
+            [kees.mb.transform :as transform]
+            [kees.mb.util :as util]
+            [selmer.parser :as selmer]))
 
 (comment
-  (map transform/sanitize (data/channel-contents {:slug "hardly"})))
+  (selmer/cache-off!)
+
+  (mapv transform/sanitize (data/channel-contents {:slug "hardly"}))
+  
+  (->> (data/channel-contents {:slug "hardly"})
+       (mapv transform/sanitize)
+       (util/pretty-spit "sample.edn"))
+  
+  (defonce sample-image
+    (second
+     (mapv transform/sanitize
+           (data/channel-contents {:slug "hardly"}))))
+  
+  (->> (selmer/render-file "block/image.html" sample-image)
+       (spit "sample.html")))
--- src/kees/mb/util.clj
+++ src/kees/mb/util.clj
@@ -1,7 +1,12 @@
-(ns kees.mb.util)
+(ns kees.mb.util
+  (:require [clojure.pprint]))
 
 (defn ceil
   [n]
   (if (and (int? n) (== n (int n)))
     n
     (inc (int n))))
+
+(defn pretty-spit
+  [f s]
+  (->> s clojure.pprint/pprint with-out-str (spit f)))
